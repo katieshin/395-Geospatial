864993d9d239e34032d2bf847829a5d219ff2468
cga815 <carolinealexander2018@u.northwestern.edu>
2017-02-21 21:20:57 -0600

    final submission

--- Project2/helper_functions.py
+++ Project2/helper_functions.py
@@ -1,4 +1,5 @@
 from math import radians, cos, sin, asin, sqrt
+import numpy as np
 
 def find3Ddist(lat1,lon1,alt1,lat2,lon2,alt2):
     a=llatoecef(lat1,lon1,alt1)
@@ -7,20 +8,15 @@ def find3Ddist(lat1,lon1,alt1,lat2,lon2,alt2):
     total=0
     i = 0
     while i < 3:
+        #find distance
         ans[i]=abs((b[i]-a[i]))**(2)
         total = total + ans[i]
         i = i+1
-    h= sqrt(total)
+    distance= sqrt(total)
     altdiff = alt2-alt1
-    slope = findslope(h, altdiff)
-    return[h,slope]
-
-def findslope(h, alt):
-    slope = 0
-    dist = (h**2 + alt**2) **(.5)
-    if dist!=0:
-        slope = alt/dist
-    return slope
+    slope = altdiff/distance
+    print "slope" + str(slope)
+    return [distance,slope]
 
 def llatoecef(lat,lon,alt):
     a= 6378137 #in m
@@ -29,9 +25,9 @@ def llatoecef(lat,lon,alt):
     e= (f*(2-f))**(.5)
     N= a/((1-((e**2)*((sin(lat))**2)))**(.5))
 
-    x= alt+N*cos(lon)*cos(lat)
-    y= alt+N*cos(lon)*sin(lat)
-    z= alt+(1-(e**2)*N*sin(lon))
+    x= (alt+N)*cos(lon)*cos(lat)
+    y= (alt+N)*cos(lon)*sin(lat)
+    z= (alt+(1-(e**2)*N)*sin(lon))
     return [x,y,z]
 
 def haversine(lat1, lon1, lat2, lon2):
@@ -48,3 +44,36 @@ def haversine(lat1, lon1, lat2, lon2):
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a))
     r = 6371000# Radius of earth in meters
+    return r*c
+
+def perp_dist(probePoint,refPoint,nonRefPoint):
+    """Finds the perpendicular distance between probePoint and the line connecting refPoint and nonRefPoint"""
+    # Points in the form of [x,y,z]
+    # Equation for distance of 3D point to line:
+    # |vector(refPoint, probePoint) cross vector(refPoint,nonRefPoint)|/|vector(refPoint,nonRefPoint)|
+
+    # vecRefToPoint = refPoint - probePoint
+    # vecLine = refPoint - nonRefPoint
+    if refPoint == nonRefPoint:
+        return 0
+    refPoint = llatoecef(refPoint[0],refPoint[1],refPoint[2])
+    nonRefPoint = llatoecef(nonRefPoint[0],nonRefPoint[1],nonRefPoint[2])
+    probePoint = llatoecef(probePoint[0],probePoint[1],probePoint[2])
+    vecRefToPoint = vector(refPoint,probePoint)
+    vecLine = vector(refPoint,nonRefPoint)
+    cross_product = np.cross(vecRefToPoint,vecLine)
+    numerator = magnitude(cross_product)
+    denominator = magnitude(vecLine)
+    return numerator/denominator
+
+def vector(point1,point2):
+    """Finds the vector between two points"""
+    vector = [0,0,0]
+    for i in range(3):
+        vector[i] = point1[i] - point2[i]
+    return vector
+
+def magnitude(vector):
+    """Finds the magnitude of a vector"""
+    y = [i**2 for i in vector]
+    return sqrt(sum(y))
--- Project2/mapmatching.py
+++ Project2/mapmatching.py
@@ -3,117 +3,133 @@ import pdb
 import csv
 import numpy
 from math import tan, degrees
-from helper_functions import haversine, find3Ddist
+from helper_functions import haversine, find3Ddist, perp_dist
+import time
+
+start_time = time.time()
 
 link_file = open("link_data.csv", "r")
 probe_file = open("probe_point_data.csv", "r")
-filewrite = open("mapmatchingoutput.csv", "w")
+slopedata = open("slopedata.csv", "w")
+matchedpoints = open("mapmatchingoutput.csv", "w")
 
 probe_data = csv.reader(probe_file, delimiter=',')
 link_data = csv.reader(link_file, delimiter=',')
-filewrite.write("linkID, distance, ourSlope, givenSlope \n")
+slopedata.write("linkID, dist_from_refnode, slope accuracy \n")
+matchedpoints.write("sampleID, dateTime, sourceCode, latitude, longitude,"+ 
+                    "altitude, speed, heading, linkPVID, direction,"+ 
+                    "distFromRef, distFromLink\n")
 # for each probe data line, create a p_lat_lng tuple and find the
 # link that contains a point with the minimum distance for that
 # lat_lng tuple
 
 # first ID = [ID,lat,long,alt]
 ID1 = [0,0,0,0]
-
 # last ID = [ID,lat,long,alt]
 ID2 = [0,0,0,0]
-
 #temp variable
 current = [0,0,0,0]
-i = 0
+i=0
+dist_from_refnode=0
+
 for p_line in probe_data:
     if i < 59:
         i = i + 1
         continue
 
     link_file.seek(0)
-    p_lat_lng = [float(p_line[3]), float(p_line[4])]
+    plat = float(p_line[3])
+    plon = float(p_line[4])
+    palt = float(p_line[5])
+    pid = p_line[0]
     dist_from_refnode = 0
-    min_distance = [0, float("infinity")]
-
-# split each link line's 15th element, which are the reference points
-# into a list using the operator "|"
+    lmin_distance_id = ""
+    lmin_distance=float("infinity")
+    # split each link line's 15th element, which are the reference points
+    # into a list using the operator "|"
 
     for l_line in link_data:
         nodes = l_line[14].split("|")
 
-# ref node is the first node in the l_line index 14, so get its lat lng
+        # ref node is the first node in the l_line index 14, so get its lat lng
+        #perp_dist([plat, plon, palt],[reflat, reflon, refalt],[nonreflat, nonreflon, nonrefalt])
 
-        refnode = [float(nodes[0].split("/")[0]),
-                   float(nodes[0].split("/")[1])]
+        
+        try:
+            ralt = float(nodes[0].split("/")[2])
+        except:
+            ralt = 0
 
-# split each links reference points string into a list of lat_lng_alt
-# strings
+        try:
+            nralt = float(nodes[len(nodes)-1].split("/")[2])
+        except:
+            nralt = 0
 
         for node in nodes:
-            l_lat_lng = [float(node.split("/")[0]),
-                         float(node.split("/")[1])]
+            llat = float(node.split("/")[0])
+            llon = float(node.split("/")[1])
+            
+            dist = float(haversine(plat,
+                                   plon,
+                                   llat,
+                                   llon))
 
-            dist = float(haversine(p_lat_lng[0],
-                                   p_lat_lng[1],
-                                   l_lat_lng[0],
-                                   l_lat_lng[1]))
+            if dist < lmin_distance:
+                lmin_distance_id = l_line[0] #line ID
+                lmin_distance_info = l_line[14]
+                lmin_distance = dist
+                refnode = [float(nodes[0].split("/")[0]),float(nodes[0].split("/")[1])]
+                nonrefnode = [float(nodes[len(nodes)-1].split("/")[0]),float(nodes[len(nodes)-1].split("/")[1])]
 
-            if dist < min_distance[1]:
-                min_distance = [l_line[0], dist]
+                rlat = refnode[0]
+                rlon = refnode[1]
+                nrlat = nonrefnode[0]
+                nrlon = nonrefnode[1]
 
-# calculate dist_from_refnode everytime updating min_distance
+                # calculate dist_from_refnode everytime updating min_distance
 
-                dist_from_refnode =  float(haversine(p_lat_lng[0],
-                                                     p_lat_lng[1],
+                dist_from_refnode =  float(haversine(plat,
+                                                     plon,
                                                      refnode[0],
                                                      refnode[1]))
+            perpd = perp_dist([plat,plon,palt],[rlat,rlon,ralt],[nrlat,nrlon,nralt])
+            
 
-    current = [min_distance[0],float(p_line[3]),float(p_line[4]),float(p_line[5])]
-    # print "printing current"
-    # print current
-    if ID1[0] == 0:
+
+    current = [lmin_distance_id,plat,plon,palt]
+
+    #calculating slope
+    if ID1[0] == 0: #first case
         ID1 = current
         ID2 = current
     else:
-        # compare
+        # if have reached a new link
         if ID1[0] != current[0]:
-            #calculate slope
-            
-            [distance,slope]=find3Ddist(ID1[1],
-                                       ID1[2],
-                                       ID1[3],
-                                       ID2[1],
-                                       ID2[2],
-                                       ID2[3])
-            # x = float(haversine(ID1[1],
-            #            ID1[2],
-            #            ID2[1],
-            #            ID2[2]))
-            # # print "printing x"
-            # # print x
-            # y = ID2[3]-ID1[3]
-            # # print "printing y"
-            # # print y
-            # # print ('printing slope')
-            # try:
-            #     slope = float(y/x)
-            # except:
-            #     slope = 0
-            #     pass
-            print slope
-            filewrite.write(str(min_distance[0]) + "," + str(dist_from_refnode)+ "," + str(slope)+ "," + str(l_line[16]) + "\n")
+            dif = ""
+            points = lmin_distance_info.split("|")
+            #if altitude data exists for that link (can calculate actual slope)
+            if (len(points)>=3) and len(points[0])>=3:
+                ref = points[0].split("/")
+                nonref = points[len(points)-1].split("/")
+                if ref[2]!= "":
+                    [actualdistance,actualslope] = find3Ddist(float(ref[0]),float(ref[1]),float(ref[2]),float(nonref[0]),float(nonref[1]),float(nonref[2]))
+                    [mydistance,myslope]=find3Ddist(ID1[1],
+                                               ID1[2],
+                                               ID1[3],
+                                               ID2[1],
+                                               ID2[2],
+                                               ID2[3])
+                dif = abs(actualslope - myslope)
+                print "dif" + str(dif)
+
             ID1 = current
-            # ID1 = current
-            # ID2 = current
+            slopedata.write(str(lmin_distance_id) + "," + str(dist_from_refnode)+ "," + str(dif)+ "," + "\n")
         else:
             ID2 = current
-            # print "printing ID2"
-            # print ID2
-
-
-
-
-    print p_lat_lng, min_distance, dist_from_refnode
-
+    #end of calculating slope
+    print "writing"
+    matchedpoints.write(str(p_line[0])+ "," + str(p_line[1])+ "," + str(p_line[2])+ "," + str(p_line[3])+ "," + str(p_line[4])+ "," + 
+                        str(p_line[5])+ "," + str(p_line[6])+ "," + str(p_line[7])+ "," + str(lmin_distance_id) + ",F," +  str(dist_from_refnode) + "," + str(perpd) + "\n")
+print("--- %s seconds ---" % (time.time() - start_time))
 probe_data.close()
 link_data.close()
